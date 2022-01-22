94fc10a7c657733294e3a8669441f9ab9c104436
El Giorgio <jruanova1987@gmail.com>
2022-01-22 19:01:35 -0300

    first commit3

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { MultipleCustomHooks } from '../src/components/03-examples/MultipleCustomHooks';
+import { MultipleCustomHooks } from '../src/components/03-examples/MultipleCustomHooks';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import {HookApp} from '../src/HookApp';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import {FocusScreen} from '../src/components/04-useRef/FocusScreen'
-import {RealExampleRef} from '../src/components/04-useRef/RealExampleRef'
+// import {RealExampleRef} from '../src/components/04-useRef/RealExampleRef'
 
 ReactDOM.render(
-  <RealExampleRef />,
+  <MultipleCustomHooks />,
   document.getElementById('app')
 );
