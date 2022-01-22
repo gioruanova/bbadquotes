b8c49521bf5aaab6bf26c0bafe78a16ed6a54939
El Giorgio <jruanova1987@gmail.com>
2022-01-22 14:23:43 -0300

    chanages6

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MultipleCustomHooks } from '../src/components/03-examples/MultipleCustomHooks';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import {HookApp} from '../src/HookApp';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
-import { MultipleCustomHooks } from '../src/components/03-examples/MultipleCustomHooks';
 // import {FocusScreen} from '../src/components/04-useRef/FocusScreen'
 // import {RealExampleRef} from '../src/components/04-useRef/RealExampleRef'
 
